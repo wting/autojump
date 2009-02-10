ba0b05cc43f9dcf372b82948ee78d0bce8488011
Joel Schaerer <joelthelion@laposte.net>
2009-02-10 17:18:40 +0100

    fix in case prompt command isn't set

--- autojump.sh
+++ autojump.sh
@@ -1,5 +1,5 @@
 if [ $SHELL = "/bin/bash" ]; then
-    export PROMPT_COMMAND="$PROMPT_COMMAND"';autojump -a "$(pwd -P)"'
+    export PROMPT_COMMAND='autojump -a "$(pwd -P)";'"$PROMPT_COMMAND"
     alias jstat="autojump --stat"
     function j { new_path=$(autojump $@);if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; echo; cd "$new_path";fi }
 fi
