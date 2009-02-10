6338b2c128e179f175ab8c70cf8465a805d00f9f
Joel Schaerer <joelthelion@laposte.net>
2009-02-10 13:54:10 +0100

    changes for distribution

--- autojump.sh
+++ autojump.sh
@@ -1,5 +1,5 @@
-#Add the following lines to your .bashrc for autojump to work
-
-export PROMPT_COMMAND='autojump.py -a "$(pwd -P)"'
-alias jstat="autojump.py --stat"
-function j { new_path=$(autojump.py $@);if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; echo; cd "$new_path";ls -ltr --color=yes| tail -n 60;fi }
+if [ $SHELL = "/bin/bash" ]; then
+    export PROMPT_COMMAND="$PROMPT_COMMAND"';autojump -a "$(pwd -P)"'
+    alias jstat="autojump --stat"
+    function j { new_path=$(autojump $@);if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; echo; cd "$new_path";fi }
+fi
