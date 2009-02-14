90ba17c945b64ecce2167b1501759fcebf652663
Joel Schaerer dedibox <joel.schaererl@laposte.net>
2009-02-14 20:07:22 +0100

    tentative fix for jackowayed's bug

--- autojump.sh
+++ autojump.sh
@@ -1,4 +1,6 @@
-if [ $SHELL = "/bin/bash" ]; then
+#This shell snippet sets the prompt command and the necessary aliases
+#Only excecute if the shell is bash and it is interactive
+if [ $SHELL = "/bin/bash" ] && [ -n "$PS1" ]; then
     export PROMPT_COMMAND='autojump -a "$(pwd -P)";'"$PROMPT_COMMAND"
     alias jumpstat="autojump --stat"
     function j { new_path=$(autojump $@);if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; echo; cd "$new_path";fi }
