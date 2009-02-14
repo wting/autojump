478da5b4f838adbb2970e09d531e41435254d56d
Daniel Jackoway <daniel@daniel-laptop.(none)>
2009-02-14 12:00:54 -0500

    made install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo cp autojump /usr/bin/
+sudo cp autojump.sh /etc/profile.d/
+sudo cp autojump.1 /usr/share/man/man1/
+# I guess I could do the source-ing here too, but I'm not sure it's a good call. 
