bcd5476c6d7e61d20b1decd8c45c4bc3ff3b5928
Joël Schaerer <joel.schaerer@insa-lyon.fr>
2009-02-18 15:13:29 +0100

    merge autojump_completion into autojump.sh ; fix atomic write ; avoid risk of extreme slowdown by multiple autojumps in prompt command

--- autojump.sh
+++ autojump.sh
@@ -1,7 +1,16 @@
 #This shell snippet sets the prompt command and the necessary aliases
 #Only excecute if the shell is bash and it is interactive
 if [ $SHELL = "/bin/bash" ] && [ -n "$PS1" ]; then
-    export PROMPT_COMMAND='autojump -a "$(pwd -P)";'"$PROMPT_COMMAND"
+    _autojump() 
+    {
+            local cur
+            COMPREPLY=()
+            cur=${COMP_WORDS[1]}
+            IFS=$'\n' read -d '' -a COMPREPLY < <(autojump --completion "$cur")
+            return 0
+    }
+    complete -F _autojump j
+    export PROMPT_COMMAND='autojump -a "$(pwd -P)"'
     alias jumpstat="autojump --stat"
     function j { new_path="$(autojump $@)";if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; echo; cd "$new_path";fi }
 fi
