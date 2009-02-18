3d4f8a4d4f21d8391fa955257a2a1112661bfba5
Joël Schaerer <joel.schaerer@insa-lyon.fr>
2009-02-18 14:55:50 +0100

    fix for directories with spaces

--- autojump.sh
+++ autojump.sh
@@ -3,5 +3,5 @@
 if [ $SHELL = "/bin/bash" ] && [ -n "$PS1" ]; then
     export PROMPT_COMMAND='autojump -a "$(pwd -P)";'"$PROMPT_COMMAND"
     alias jumpstat="autojump --stat"
-    function j { new_path=$(autojump $@);if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; echo; cd "$new_path";fi }
+    function j { new_path="$(autojump $@)";if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; echo; cd "$new_path";fi }
 fi
