0dceb14a89524ca4c99a4b95f64c790a0ff97031
Joel Schaerer <joelthelion@laposte.net>
2009-02-20 04:55:22 +0100

    make autojump.sh POSIX compliant, thanks oylenshpeegul

--- autojump.sh
+++ autojump.sh
@@ -1,5 +1,5 @@
 #Source autojump.bashrc only if we're on bash, as it is
 #not compatible with other shells
-if [ $SHELL = "/bin/bash" ] && [ -n "$PS1" ]; then
-	source /etc/profile.d/autojump.bash
+if [ $BASH_VERSION ] && [ -n "$PS1" ]; then
+	. /etc/profile.d/autojump.bash
 fi
