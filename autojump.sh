b21f004061f8b06492c20d8daafa6b5a8a55024e
Joel Schaerer <joelthelion@laposte.net>
2009-03-03 12:09:36 +0100

    changes for better gdm compatibility

--- autojump.sh
+++ autojump.sh
@@ -1,5 +1,5 @@
 #Source autojump.bashrc only if we're on bash, as it is
 #not compatible with other shells
-if [ "$BASH_VERSION" ] && [ -n "$PS1" ]; then
+if [ "$BASH_VERSION" ] && [ -n "$PS1" ] && echo $SHELLOPTS | grep -v posix >>/dev/null; then
 	. /etc/profile.d/autojump.bash
 fi
