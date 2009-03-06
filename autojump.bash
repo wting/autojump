a6fb3b7fe7cc549e718234191bebf79999f7e8a1
Joel Schaerer <joelthelion@laposte.net>
2009-03-06 20:39:02 +0100

    ok this might even be a better option to run the prompt command in the background

--- autojump.bash
+++ autojump.bash
@@ -9,6 +9,6 @@ _autojump()
         return 0
 }
 complete -F _autojump j
-export PROMPT_COMMAND='autojump -a "$(pwd -P)"'
+export PROMPT_COMMAND='(autojump -a "$(pwd -P)"&)>/dev/null'
 alias jumpstat="autojump --stat"
 function j { new_path="$(autojump $@)";if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; echo; cd "$new_path";fi }
