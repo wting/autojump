29046f4dce07dd8d97902f491cb54a8897e2053f
Joel Schaerer <joelthelion@laposte.net>
2009-03-08 15:37:42 +0100

    remove spurious newline

--- autojump.bash
+++ autojump.bash
@@ -11,4 +11,4 @@ _autojump()
 complete -F _autojump j
 export PROMPT_COMMAND='(autojump -a "$(pwd -P)"&)>/dev/null'
 alias jumpstat="autojump --stat"
-function j { new_path="$(autojump $@)";if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; echo; cd "$new_path";fi }
+function j { new_path="$(autojump $@)";if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; cd "$new_path";fi }
