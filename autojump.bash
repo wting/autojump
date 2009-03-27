0136d8f138537ef0e16b1f19e1c7fd350b867e1e
Simon Marache-Francisco <marache@IV-CL-2008-097.creatis.insa-lyon.fr>
2009-03-27 16:54:55 +0100

    Just a Test

--- autojump.bash
+++ autojump.bash
@@ -1,4 +1,7 @@
 #This shell snippet sets the prompt command and the necessary aliases
+
+
+# TODO
 _autojump() 
 {
         local cur
@@ -9,6 +12,12 @@ _autojump()
         return 0
 }
 complete -F _autojump j
-export PROMPT_COMMAND='(autojump -a "$(pwd -P)"&)>/dev/null'
+
+function preexec() {
+    autojump -a "$(pwd -P)"&)>/dev/null
+}
+
+# OK
+
 alias jumpstat="autojump --stat"
 function j { new_path="$(autojump $@)";if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; cd "$new_path";fi }
