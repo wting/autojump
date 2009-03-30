c43c7b73b5d7f5f328a4c1c57587b95fa9b70ded
Simon Marache-Francisco <marache@IV-CL-2008-097.creatis.insa-lyon.fr>
2009-03-30 09:24:46 +0200

    added autojump.zsh to profile.d and autojump in zshrc

--- autojump.bash
+++ autojump.bash
@@ -1,7 +1,4 @@
 #This shell snippet sets the prompt command and the necessary aliases
-
-
-# TODO
 _autojump() 
 {
         local cur
@@ -12,12 +9,6 @@ _autojump()
         return 0
 }
 complete -F _autojump j
-
-function preexec() {
-    autojump -a "$(pwd -P)"&)>/dev/null
-}
-
-# OK
-
+export PROMPT_COMMAND='(autojump -a "$(pwd -P)"&)>/dev/null'
 alias jumpstat="autojump --stat"
 function j { new_path="$(autojump $@)";if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; cd "$new_path";fi }
