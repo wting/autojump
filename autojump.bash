264eea2756995c58b2aeb0ee2c051f5797b317a4
Joël Schaerer <joel.schaerer@insa-lyon.fr>
2009-04-10 13:00:20 +0200

    Preserve the original PROMPT_COMMAND: thanks to "courts"
    for the original implementation
    (http://n2.nabble.com/Preserving-COMMAND_PROMPT-td2507773.html)

--- autojump.bash
+++ autojump.bash
@@ -9,6 +9,9 @@ _autojump()
         return 0
 }
 complete -F _autojump j
-export PROMPT_COMMAND='(autojump -a "$(pwd -P)"&)>/dev/null'
+AUTOJUMP='(autojump -a "$(pwd -P)"&)>/dev/null'
+if ! [[ $PROMPT_COMMAND =~ autojump ]]; then
+  export PROMPT_COMMAND="$PROMPT_COMMAND && $AUTOJUMP"
+fi 
 alias jumpstat="autojump --stat"
 function j { new_path="$(autojump $@)";if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; cd "$new_path";fi }
