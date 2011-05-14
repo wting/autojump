7add5c601ed58fbfc24d17c3c4c437da2742bd73
William Ting <hp@williamting.com>
2011-05-14 00:09:02 -0500

    modify install script to support local installs

--- autojump.bash
+++ autojump.bash
@@ -25,26 +25,14 @@ _autojump()
         done  < <(autojump --bash --completion $cur)
 }
 complete -F _autojump j
-#data_dir=${XDG_DATA_HOME:-$([ -e ~/.local/share ] && echo ~/.local/share || echo ~)}
-data_dir=$([ -e ~/.local/share ] && echo ~/.local/share || echo ~)
+
 export AUTOJUMP_HOME=${HOME}
-if [[ "$data_dir" = "${HOME}" ]]
-then
-    export AUTOJUMP_DATA_DIR=${data_dir}
-else
-    export AUTOJUMP_DATA_DIR=${data_dir}/autojump
-fi
-if [ ! -e "${AUTOJUMP_DATA_DIR}" ]
-then
-    mkdir "${AUTOJUMP_DATA_DIR}"
-    mv ~/.autojump_py "${AUTOJUMP_DATA_DIR}/autojump_py" 2>>/dev/null #migration
-    mv ~/.autojump_py.bak "${AUTOJUMP_DATA_DIR}/autojump_py.bak" 2>>/dev/null
-    mv ~/.autojump_errors "${AUTOJUMP_DATA_DIR}/autojump_errors" 2>>/dev/null
-fi
+export AUTOJUMP_DATA_DIR=${HOME}/.autojump
 
 AUTOJUMP='{ [[ "$AUTOJUMP_HOME" == "$HOME" ]] && (autojump -a "$(pwd -P)"&)>/dev/null 2>>${AUTOJUMP_DATA_DIR}/autojump_errors;} 2>/dev/null'
 if [[ ! $PROMPT_COMMAND =~ autojump ]]; then
-  export PROMPT_COMMAND="${PROMPT_COMMAND:-:} ; $AUTOJUMP"
+    export PROMPT_COMMAND="${PROMPT_COMMAND:-:} ; $AUTOJUMP"
 fi
+
 alias jumpstat="autojump --stat"
-function j { new_path="$(autojump $@)";if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; cd "$new_path" && ls;else false; fi }
+function j { new_path="$(autojump $@)";if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; cd "$new_path"; else false; fi }
