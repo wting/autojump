b318e11a6a53770212b17b39a139b25c5fbdc4d1
xiaonaitong <xiaonaitong@gmail.com>
2011-09-20 18:45:42 +0800

    don't use --bash option in function j

--- autojump.bash
+++ autojump.bash
@@ -52,4 +52,4 @@ case $PROMPT_COMMAND in
 esac
 
 alias jumpstat="autojump --stat"
-function j { new_path="$(autojump --bash $@)";if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; cd "$new_path";else false; fi }
+function j { new_path="$(autojump $@)";if [ -n "$new_path" ]; then echo -e "\\033[31m${new_path}\\033[0m"; cd "$new_path";else false; fi }
