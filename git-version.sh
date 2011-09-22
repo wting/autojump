2350da51e5a9e476e5df9986a26823e19740ca52
Johnny Kuan <jkuan@ece.ubc.ca>
2011-09-22 02:06:08 -0700

    use git describe instead of rev-parse

--- git-version.sh
+++ git-version.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 # add git revision to autojump
-gitrevision=`git rev-parse HEAD`
+gitrevision=`git describe`
 if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then
     gitrevision=$gitrevision"-dirty"
 fi
 sed -e "s/^AUTOJUMP_VERSION = \".*\"$/AUTOJUMP_VERSION = \"git revision $gitrevision\"/" autojump > autojump-version
 mv -f autojump-version autojump
 chmod a+rw autojump
-
-
