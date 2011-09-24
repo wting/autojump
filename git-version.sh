46ba7d61f0b4fa9552e0960125f3ca14541d6353
Joël Schaerer <joel.schaerer@gmail.com>
2011-09-24 11:54:40 +0200

    avoid failing when the user didn't install from git

--- git-version.sh
+++ git-version.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 # add git revision to autojump
+
+# Fail silently if there is no git directory, ie. if the user installed from a regular download
+if [[ ! -d .git ]]
+then
+    exit
+fi
+
 gitrevision=`git describe`
 if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then
     gitrevision=$gitrevision"-dirty"
