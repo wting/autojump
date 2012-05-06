da3d660b5094019b459ecfcc6e903b0812cf20f7
William Ting <william.h.ting@gmail.com>
2012-05-06 12:56:01 -1000

    Remove license from individual files, consolidate information into AUTHORS and LICENSE

--- bin/autojump.sh
+++ bin/autojump.sh
@@ -1,19 +1,4 @@
 # Source autojump on BASH or ZSH depending on the shell
-#Copyright Joel Schaerer 2008, 2009
-#This file is part of autojump
-
-#autojump is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
-#
-#autojump is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#
-#You should have received a copy of the GNU General Public License
-#along with autojump.  If not, see <http://www.gnu.org/licenses/>.
 if [ "$BASH_VERSION" ] && [ -n "$PS1" ] && echo $SHELLOPTS | grep -v posix >>/dev/null; then
     if [ -f ~/.autojump/etc/profile.d/autojump.bash ]; then
         source ~/.autojump/etc/profile.d/autojump.bash
