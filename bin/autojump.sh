837671fe0bb558c186157992c08cfc84fd0f5908
William Ting <william.h.ting@gmail.com>
2012-11-22 14:15:28 -0600

    refactoring

--- bin/autojump.sh
+++ bin/autojump.sh
@@ -1,14 +1,13 @@
-# Source autojump on BASH or ZSH depending on the shell
-if [ "$BASH_VERSION" ] && [ -n "$PS1" ] && echo $SHELLOPTS | grep -v posix >>/dev/null; then
-    if [ -f ~/.autojump/etc/profile.d/autojump.bash ]; then
-        source ~/.autojump/etc/profile.d/autojump.bash
-    elif [ -f /etc/profile.d/autojump.bash ]; then
-        source /etc/profile.d/autojump.bash
-    fi
-elif [ "$ZSH_VERSION" ] && [ -n "$PS1" ]; then
-    if [ -f ~/.autojump/etc/profile.d/autojump.zsh ]; then
-        source ~/.autojump/etc/profile.d/autojump.zsh
-    elif [ -f /etc/profile.d/autojump.zsh ]; then
-        source /etc/profile.d/autojump.zsh
-    fi
+# source autojump on BASH or ZSH depending on the shell
+
+shell=`echo ${SHELL} | awk -F/ '{ print $NF }'`
+
+# check local install
+if [ -s ~/.autojump/etc/profile.d/autojump.${shell} ]; then
+	source ~/.autojump/etc/profile.d/autojump.${shell}
+
+# check global install
+elif [ -s /etc/profile.d/autojump.${shell} ]; then
+	source /etc/profile.d/autojump.${shell}
+
 fi
