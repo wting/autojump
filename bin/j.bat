1d13c0d4aef91740f0c406a294bcee3c24010138
Michael Lawson <mdlawson@gmx.com>
2014-01-13 19:04:39 +0000

    fixed batch variable handling

--- bin/j.bat
+++ bin/j.bat
@@ -1,15 +1,21 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 echo %*|>nul findstr /rx \-.*
 if ERRORLEVEL 1 (
-  for /f %%G in ('python %~dp0\autojump %*') do set new_path=%%G
-  if exist %new_path%/nul (
-    cd %new_path%
+  for /f %%i in ('python %~dp0\autojump %*') do set new_path=%%i
+  if exist !new_path!\nul (
+    echo !new_path!
+	pushd !new_path!
+	REM endlocal is necessary so that we can change directory for outside of this script
+	REM but will automatically popd. We mush pushd twice to work around this. 
+	pushd !new_path!
+	endlocal
+	popd
   ) else (
     echo autojump: directory %* not found
     echo try `autojump --help` for more information
   )
-  echo %new_path%
 ) else (
   python %~dp0\autojump %* 
 )
--- bin/jo.bat
+++ bin/jo.bat
@@ -1,15 +1,15 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 echo %*|>nul findstr /rx \-.*
 if ERRORLEVEL 1 (
-  for /f %%G in ('python %~dp0\autojump %*') do set new_path=%%G
-  if exist %new_path%/nul (
-    start %new_path%
+  for /f %%i in ('python %~dp0\autojump %*') do set new_path=%%i
+  if exist !new_path!\nul (
+    start !new_path!
   ) else (
     echo autojump: directory %* not found
     echo try `autojump --help` for more information
   )
-  echo %new_path%
 ) else (
   python %~dp0\autojump %* 
 )
