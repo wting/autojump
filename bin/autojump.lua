7a756aaa4e1a30abb30a3869293ca1008e732607
Michael Lawson <mdlawson@gmx.com>
2014-01-15 13:05:45 +0000

    add completion support

--- bin/autojump.lua
+++ bin/autojump.lua
@@ -6,3 +6,15 @@ function autojump_add_to_database()
 end
 
 clink.prompt.register_filter(autojump_add_to_database, 99)
+
+function autojump_completion(word)
+  for line in io.popen("python " .. AUTOJUMP_BIN .. " --complete " .. word):lines() do
+    clink.add_match(line)
+  end
+  return {} 
+end
+
+local autojump_parser = clink.arg.new_parser()
+autojump_parser:set_arguments({ autojump_completion })
+
+clink.arg.register_parser("j", autojump_parser)
