f480bd131576da5933b58dd4434bb79684da82ef
William Ting <wting@yelp.com>
2014-10-04 20:27:51 -0700

    Detect user shell as opposed to login shell.
    Fixes #256.

--- bin/autojump.sh
+++ bin/autojump.sh
@@ -1,6 +1,16 @@
-# source autojump on BASH or ZSH depending on the shell
-
-shell=$(echo ${SHELL} | awk -F/ '{ print $NF }')
+# the login $SHELL isn't always the one used
+# NOTE: problems might occur if /bin/sh is symlinked to /bin/bash
+if [ -n "${BASH}" ]; then
+    shell="bash"
+elif [ -n "${ZSH_NAME}" ]; then
+    shell="zsh"
+elif [ -n "${__fish_datadir}" ]; then
+    shell="fish"
+elif [ -n "${version}" ]; then
+    shell="tcsh"
+else
+    shell=$(echo ${SHELL} | awk -F/ '{ print $NF }')
+fi
 
 # prevent circular loop for sh shells
 if [ "${shell}" = "sh" ]; then
