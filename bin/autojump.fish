10dede00921b63c3b07d1b873be68bce0e7cfc7d
Leonid Shevtsov <leonid@shevtsov.me>
2014-11-18 10:55:21 +0200

    Fix fish shell conditional syntax

--- bin/autojump.fish
+++ bin/autojump.fish
@@ -11,7 +11,7 @@ complete -x -c j -a '(autojump --complete (commandline -t))'
 
 
 # set error file location
-if (uname) == "Darwin"
+if test (uname) = "Darwin"
     set -x AUTOJUMP_ERROR_PATH ~/Library/autojump/errors.log
 else if test -d $XDG_DATA_HOME
     set -x AUTOJUMP_ERROR_PATH $XDG_DATA_HOME/autojump/errors.log
