b2f243ee17e918821e2d15a1de5e20b0f7f981f2
William Ting <io@williamting.com>
2016-09-30 12:10:07 -0700

    Revert "Merge remote-tracking branch 'sodiumjoe/optimize-startup'"
    This reverts commit 39990fff58f4141bf60cebd30d93954bae84828a, reversing
    changes made to c4d82b1888059f9d210e0db25a0e444de8e3dbcf.

--- bin/autojump.zsh
+++ bin/autojump.zsh
@@ -10,9 +10,8 @@ fi
 
 
 # set homebrew installation paths
-local brew_prefix=${BREW_PREFIX:-$(brew --prefix)}
-if [[ -d "${brew_prefix}/share/zsh/site-functions" ]]; then
-    fpath=("${brew_prefix}/share/zsh/site-functions" ${fpath})
+if command -v brew &>/dev/null && [[ -d "$(brew --prefix)/share/zsh/site-functions" ]]; then
+    fpath=("$(brew --prefix)/share/zsh/site-functions" ${fpath})
 fi
 
 
