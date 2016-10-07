b0bc5348386569871d7992b0ee3c2f4313924f1d
Moritz Flucht <m.flucht@gmail.com>
2016-10-07 15:18:03 +0200

    Fix jumping to paths containing spaces on Windows.

--- bin/j.bat
+++ bin/j.bat
@@ -3,15 +3,15 @@ setlocal EnableDelayedExpansion
 
 echo %*|>nul findstr /rx \-.*
 if ERRORLEVEL 1 (
-  for /f %%i in ('python "%~dp0\autojump" %*') do set new_path=%%i
+  for /f "delims=" %%i in ('python "%~dp0\autojump" %*') do set new_path=%%i
   if exist !new_path!\nul (
     echo !new_path!
-	pushd !new_path!
-	REM endlocal is necessary so that we can change directory for outside of this script
-	REM but will automatically popd. We mush pushd twice to work around this. 
-	pushd !new_path!
-	endlocal
-	popd
+    pushd !new_path!
+    REM endlocal is necessary so that we can change directory for outside of this script
+    REM but will automatically popd. We mush pushd twice to work around this.
+    pushd !new_path!
+    endlocal
+    popd
   ) else (
     echo autojump: directory %* not found
     echo try `autojump --help` for more information
