4f07c55f033d6794775a44cd7fa02f60ceafe0c4
Alex Thiessen <alex.thiessen.de+github@gmail.com>
2022-02-08 21:10:40 +0100

    tests/integration: Add a failing test
    Reproduce https://github.com/wting/autojump/issues/348

---
+++ tests/integration/autojump_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+sys.path.append(os.path.join(os.getcwd(), 'bin'))  # noqa
+from autojump import find_matches
+from autojump_data import entriefy
+
+
+def test_find_matches_returns_unique_results(tmpdir):
+    path = str(tmpdir)
+    needle = str(os.path.basename(tmpdir))
+
+    matches = find_matches(entriefy({path: 10}), [needle])
+
+    assert list(matches) == list(entriefy({path: 10}))
